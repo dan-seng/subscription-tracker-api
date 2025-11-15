7cd7d0ed94eb90685abae14554348be478cfe81b
Daniel <da16gi@gmail.com>
2025-11-15 22:55:57 +0300

    Update the update user function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-
+import bcrypt from "bcryptjs";
 
 export const getUsers = async (req, res, next) =>{
     try {
@@ -32,6 +32,7 @@ export const getUser = async (req, res, next) =>{
     }   
 }
 
+/*
 export const updateUser = async (req, res, next) =>{
     try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }).select('-password');
@@ -48,7 +49,46 @@ export const updateUser = async (req, res, next) =>{
     } catch (error) {
         next(error)
     }   
-}
+} */
+
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Update allowed fields
+    if (req.body.name) user.name = req.body.name;
+    if (req.body.email) user.email = req.body.email;
+
+    // Hash password if provided
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
+    }
+
+    await user.save();
+
+    // remove password from output
+    const userResponse = user.toObject();
+    delete userResponse.password;
+
+    res.status(200).json({
+      success: true,
+      data: userResponse,
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteUser = async (req, res, next) =>{
     try {
