f23a9e09d37eb3b42bf82da549636e2db7768225
Daniel <da16gi@gmail.com>
2025-11-17 17:11:31 +0300

    Fix emailing problem

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,15 +1,16 @@
 import nodemailer from 'nodemailer';
-
-import { EMAIL_PASSWORD } from './env.js'
+import { EMAIL_PASSWORD } from './env.js';
 
 export const accountEmail = 'dan16son@gmail.com';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,        // use 587 for TLS (not 465)
+  secure: false,    // false for port 587
   auth: {
     user: accountEmail,
-    pass: EMAIL_PASSWORD
-  }
-})
+    pass: EMAIL_PASSWORD, // your app password
+  },
+});
 
 export default transporter;
